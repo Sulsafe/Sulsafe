7d327e137133ba2c96332e10133f526b4ae94bcd
Sulsafe <sulsafetreinamentos@gmail.com>
2026-06-29 03:02:48 -0300

    Update import paths in boletim.js

--- js/views/boletim.js
+++ js/views/boletim.js
@@ -2,8 +2,8 @@
 // VIEW: BOLETIM (Aluno / Professor)
 // ============================================================
 import { S, role, uid, nav, fmtD } from '../state.js'
-import { sbGetNotasAluno, sbLancarNota, sbGetAlunos, sbGetMediasNRs } from './supabase-client.js'
-import { toast, handleError, sanitizar, openMdl, closeMdl, $, $$, NRS } from './utils.js'
+import { sbGetNotasAluno, sbLancarNota, sbGetAlunos, sbGetMediasNRs } from '../supabase-client.js'
+import { toast, handleError, sanitizar, openMdl, closeMdl, $, $$, NRS } from '../utils.js'
 
 export function vBoletim() {
     const r = role()
